51913268c9a0e03010880c4691d8b9a43ae0adb2
AndreyV0607 <andreyvargas070607@gmail.com>
2025-06-24 00:47:40 -0600

    Add files via upload

---
+++ IntroProgra_ProyectoFinal1.0/src/introprogra_proyectofinal1/pkg0/IntroProgra_ProyectoFinal10.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package introprogra_proyectofinal1.pkg0;
+
+/**
+ *
+ * @author andreyvargassolis
+ */
+public class IntroProgra_ProyectoFinal10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
