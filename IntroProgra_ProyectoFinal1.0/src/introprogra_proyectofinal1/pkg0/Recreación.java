a532269e0864f68f23df7c84757a518c987f621a
arjolaca11 <145235517+arjolaca11@users.noreply.github.com>
2025-07-05 01:09:25 -0600

    Recreación.java

--- IntroProgra_ProyectoFinal1.0/src/introprogra_proyectofinal1/pkg0/Recreación.java
+++ IntroProgra_ProyectoFinal1.0/src/introprogra_proyectofinal1/pkg0/Recreación.java
@@ -4,6 +4,30 @@
  */
 package introprogra_proyectofinal1.pkg0;
 
+public class Recreacion {
+
+    // --- Atributos de la Clase ---
+    private static final int MAX_ESPACIOS = 10;
+    private static final int MAX_RESERVAS = 100;
+    private static final int CANTIDAD_TURNOS_MESAS = 16; 
+
+    private EspacioRecreativo[] espacios;
+    private Reserva[] reservas;
+    private String[][] matrizHorariosMesas; 
+    private String[] etiquetasTurnos;
+
+    private int numEspacios;
+    private int numReservas;
+
+    private Usuario[] listaGeneralDeUsuarios;
+    private int cantidadTotalUsuarios;
+
+    /**
+     * Constructor que inicia el apartado de Recreación.
+     * @param usuarios El arreglo de usuarios de todo el sistema.
+     * @param cantidad La cantidad actual de usuarios en el arreglo.
+     */
+
 /**
  *
  * @author andreyvargassolis
